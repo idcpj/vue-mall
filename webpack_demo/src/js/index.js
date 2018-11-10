a62cfdd114617c8aa1b89c96afe73abe4db4e718
idcpj <260083304@qq.com>
2018-11-10 19:34:21 +0800

    添加

--- webpack_demo/src/js/index.js
+++ webpack_demo/src/js/index.js
@@ -1,3 +1,4 @@
+import "../css/index.css";
 require(['./common.js'],function (common) {
     common.initIndex();
 });
--- webpack_demo/webpack.config.js
+++ webpack_demo/webpack.config.js
@@ -1,8 +1,11 @@
-var path = require("path");
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
-        index:"./src/js/index.js",
+        index:"./src/js/index.js", //设置多入口
         cart:'./src/js/cart.js',
     },
     output: {
@@ -11,10 +14,29 @@ module.exports = {
         // publicPath: "http://www.xxx.com/xx",//加前缀
     },
     module:{
-
+        rules:[
+            {
+                test:/\.css$/,
+                use:["style-loader",'css-loader']
+            }
+        ]
     },
     plugins:[
-
+        new CleanWebpackPlugin(['./dist'],{
+            root:path.join(__dirname,''),
+            verbose:true,
+            dry:false,
+        }),
+        new HtmlWebpackPlugin({
+            filename:"index.html",
+            template:"./src/index.html",
+            chunks:['index'], //只加载 指定 js
+        }),
+        new HtmlWebpackPlugin({
+            filename:"cart.html",
+            template:"./src/cart.html",
+            chunks:['cart'],
+        }),
     ],
-    devtool:"#source-map"
+    devtool:"#source-map" //用于调试
 };
